82b267972601e5289d42d87287fe5f6c6dff13dd
Alvaro Alonso <alonso.miguel.alvaro1@gmail.com>
2023-10-01 21:00:03 +0200

    Day 14

---
+++ DeleteNodeInABST.java
@@ -0,0 +1,51 @@
+/*
+ * Given a root node reference of a BST and a key, delete 
+ * the node with the given key in the BST. Return the root 
+ * node reference (possibly updated) of the BST.
+ * 
+ * Basically, the deletion can be divided into two stages:
+ * 
+ *  Search for a node to remove.
+ *  If the node is found, delete the node.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return root;
+        } 
+        if(root.val == key){
+            root= delete(root, key);
+        } else if(root.val< key){
+            root.right = deleteNode(root.right, key);
+        } else{
+            root.left = deleteNode(root.left, key);
+        } return root;
+    }
+    private TreeNode delete(TreeNode root, int key){
+        if(root.left == null && root.right == null) return null;
+        if(root.left == null) return root.right;
+        if(root.right == null) return root.left;
+        TreeNode temp = root.right;
+        while(temp.left != null){
+            temp = temp.left;
+        }
+        temp.left = root.left;
+        return root.right;
+    }
+}
